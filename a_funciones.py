39d2d83cbf16f1e3b5f42a461a07eab7fbe14e4d
Mauela Blandon <manuela.blandon@udea.edu.co>
2024-09-19 01:24:03 -0500

    Lista descripción de funciones

--- a_funciones.py
+++ a_funciones.py
@@ -1,168 +1,73 @@
-# Importación de librerías
+## Importación de librerías
 
+#Librerías para Manipulación de Datos y Operaciones Numéricas
 import numpy as np
 import pandas as pd
-from sklearn.impute import SimpleImputer # Para imputación
-from sklearn.feature_selection import SelectFromModel
-from sklearn.feature_selection import RFE
+
+#Librerías para Machine Learning y Preprocesamiento
+
+from sklearn.impute import SimpleImputer
+from sklearn.feature_selection import SelectFromModel, RFE, VarianceThreshold, SelectKBest, f_regression, mutual_info_regression, f_classif, mutual_info_classif, chi2
 from sklearn.model_selection import cross_val_predict, cross_val_score, cross_validate
-import joblib
-from sklearn.preprocessing import StandardScaler # Para escalar variables
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+
+#Librerías para Visualización
 
 import seaborn as sns
 import matplotlib.pyplot as plt
 import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 from IPython.display import display, Markdown
 
+#Otras Librerías
 
-#Librerías para métodos de filtrado
-from sklearn.feature_selection import RFE
-from sklearn.linear_model import LogisticRegression
-from sklearn.feature_selection import VarianceThreshold
-from sklearn.feature_selection import SelectKBest, f_regression, mutual_info_regression,  f_classif, mutual_info_classif, chi2
-from sklearn.preprocessing import MinMaxScaler
-
-#Librerías para
-import os
-import joblib
+import joblib # Para guardar y cargar objetos Python.
+import os # Para operaciones del sistema de archivos.
 
 # Las fucniones más útiles para el desarrollo del proyecto están en este script
 
+## Funciones y descripciones
 
-#---------------------------------------Función para ejecutar archivos .sql --------------------------------------------
+#--------------------Funciones para manipulación de archivos-------------------------------
+#Funció ejecutar_sql para leer 
 def ejecutar_sql (nombre_archivo,cur):
+  '''
+    Esta función ejecuta archivos sql desde python. para ello toma dos parámetros:
+    nombre_archivo: El nombre del archivo SQL que contiene el script a ejecutar.
+    cur: Un objeto cursor de la conexión a la base de datos, que se utiliza para ejecutar comandos SQL.
+  '''
   sql_file=open(nombre_archivo)
   sql_as_string=sql_file.read()
   sql_file.close()
   cur.executescript(sql_as_string)
 
-
-#---------------------------------------Función para eliminar archivos--------------------------------------------
-'''
-La función eliminar_archivos borra los archivos que 
-'''
+#La función eliminar_archivos elimina archivos del repositorio si existen
 def eliminar_archivos(rutas_archivos):
     """
-    Elimina los archivos especificados en la lista de rutas si existen.
-    
-    :param rutas_archivos: Lista de rutas de archivos a eliminar.
+    Elimina los archivos existentes, esto con el fin de poder realizar pruebas y no realizar el 
+    borrado de manera manual, pues la base de datos creada para guardar las tablas de sql no se puede
+    sobreescribir y por tanto no se actualizaba, entonces se decidió por implementar esta función para
+    hacerse de manera automática.
+
+    Esta función recibe como parámetro la ruta del archivo a eliminar.
     """
     for ruta in rutas_archivos:
+        #Se realiza un condicional de si existe o no la ruta para evitar errores
         if os.path.exists(ruta):
             os.remove(ruta)
             print(f"Archivo eliminado: {ruta}")
         else:
             print(f"El archivo no existe: {ruta}")
 
-
- # Función para imputar variables numéricas
- 
-def imputar_numericas (df,tipo):
-
-    if str(tipo)=='mean':
-        numericas=df.select_dtypes(include=['number']).columns
-        imp_mean=SimpleImputer(strategy='mean')
-        df[numericas]=imp_mean.fit_transform(df[numericas])
-        return df
-    
-    if str(tipo)=='most_frequent':
-        numericas=df.select_dtypes(include=['number']).columns
-        imp_mean=SimpleImputer(strategy='most_frequent')
-        df[numericas]=imp_mean.fit_transform(df[numericas])
-        return df
-
-
-# Función para imputar variables categóricas y numéricas
-
-def imputar_f (df,list_cat):  
-         
-                  
-    df_c=df[list_cat]
-
-    df_n=df.loc[:,~df.columns.isin(list_cat)]
-
-    imputer_n=SimpleImputer(strategy='median')
-    imputer_c=SimpleImputer(strategy='most_frequent')
-
-    imputer_n.fit(df_n)
-    imputer_c.fit(df_c)
-    imputer_c.get_params()
-    imputer_n.get_params()
-
-    X_n=imputer_n.transform(df_n)
-    X_c=imputer_c.transform(df_c)
-
-
-    df_n=pd.DataFrame(X_n,columns=df_n.columns)
-    df_c=pd.DataFrame(X_c,columns=df_c.columns)
-    df_c.info()
-    df_n.info()
-
-    df=pd.concat([df_n, df_c],axis = 1)
-    
-    return df
-
-
-def convertir_fecha(dataframe, columna):
-
-    dataframe[columna] = pd.to_datetime(dataframe[columna])
-
-    return dataframe.info()
-
-# RFE para la selección de variables para distintos modelos
-
-def funcion_rfe(modelos,X,y, num_variables, paso):
-  resultados = {}
-  for modelo in modelos: 
-    rfemodelo = RFE(modelo, n_features_to_select = num_variables, step = paso)
-    fit = rfemodelo.fit(X,y)
-    var_names = fit.get_feature_names_out()
-    puntaje = fit.ranking_
-    diccionario_importancia = {}
-    nombre_modelo = modelo.__class__.__name__
-
-    for i,j in zip(var_names,puntaje):
-      diccionario_importancia[i] = j
-      resultados[nombre_modelo] = diccionario_importancia
-  
-  return resultados
-
-# Diagrama de barras para despliegue de resultados 
-def histogram(df1, df2,  columna, name1, name2, color1, color2, titulo):
-
-    fig = make_subplots(rows = 1, cols = 2)
-    fig.add_trace(
-        go.Histogram(x = df1[columna], name = name1, marker_color = color1),
-        row = 1, col = 1
-    )
-
-    fig.add_trace(
-        go.Histogram(x = df2[columna], name = name2, marker_color = color2),
-        row = 1, col = 2
-    )
-
-    fig.update_layout(
-        title_text = titulo,
-        template = 'simple_white')
-    fig.show();   
-    
-# Diagrama de lineas para despliegue de resultados
-def line(df, columna1, columna2, titulo, xlabel, ylabel): 
-
-    years_1 = df.groupby([columna1])[[columna2]].count().reset_index()
-
-    fig = px.line(years_1, x = columna1, y = columna2)
-    fig.update_layout(
-        title = titulo,
-        xaxis_title = xlabel,
-        yaxis_title = ylabel,
-        template = 'simple_white',
-        title_x = 0.5)
-    fig.show()
-    
-#--------- Función para análisis descriptivo -------------
-
+#--------------------análisis exploratorio-------------------------------
+#Función para el análisis descriptivo
 def check_df(dataframe, head=5):
+    '''
+    Esta función imprime un resumen detallado del dataframe, como el número de columnas y filas que tiene,
+    el tipo de dato que es, una pequeña observación del dataframe, muestra los nulos de cada variable y 
+    si tiene duplicados.
+    '''
     display(Markdown('**Shape**'))
     display(dataframe.shape)
 
@@ -178,40 +83,47 @@ def check_df(dataframe, head=5):
     display(Markdown("**Duplicated**"))
     display(dataframe.duplicated().sum())
 
-
-#-------- Histograma o boxplot ------------
-def plot_histogram_and_boxplot(df, column_name):
+#Visualización de datos
+#Función para la generación de gráfico histograma y boxplot
+def plot_hist_box(df, list_numericas):
+    '''
+    Esta función recibe como argumentos un dataframe y una lista de nombre de variables
+    con el fin de crear un gráfico histograma y otro de boxplot para cada una de estas variables numércias.
+    '''
     fig, axs = plt.subplots(1, 2, figsize=(12, 6))
-
     color = 'lightblue'
     
     # Histograma
-    sns.histplot(df[column_name], kde=False, color=color, ax=axs[0])
-    axs[0].set_title(f'Histograma de {column_name}')
-    axs[0].set_xlabel(column_name)
+    sns.histplot(df[list_numericas], kde=False, color=color, ax=axs[0])
+    axs[0].set_title(f'Histograma de {list_numericas}')
+    axs[0].set_xlabel(list_numericas)
     axs[0].set_ylabel('Frecuencia')
     
     # Boxplot
-    sns.boxplot(x=df[column_name], color=color, ax=axs[1])
-    axs[1].set_title(f'Boxplot de {column_name}')
-    axs[1].set_xlabel(column_name)
+    sns.boxplot(x=df[list_numericas], color=color, ax=axs[1])
+    axs[1].set_title(f'Boxplot de {list_numericas}')
+    axs[1].set_xlabel(list_numericas)
 
     plt.tight_layout()
     plt.show()
 
-#----------------- Gráfico histograma -----------------
-def plot_categorical_distribution(df, column_name):
-
+#Gráfico para la generación de histogramas
+def plot_categorical_distribution(df, list_cat):
+    '''
+    Esta función recibe como argumento un dataframe y una lista de variables categóricas
+    con el fin de crear de manera iterativa un histograma para cada una de estas variables en
+    la lista.
+    '''
     # Calcular la frecuencia de cada categoría
-    data = df[column_name].value_counts().reset_index()
-    data.columns = [column_name, 'Frecuencia']
+    data = df[list_cat].value_counts().reset_index()
+    data.columns = [list_cat, 'Frecuencia']
 
     # Crear el gráfico de barras
-    fig = px.bar(data, x=column_name, y='Frecuencia',
-                 title=f'Distribución de la Variable Categórica: {column_name}',
-                 labels={column_name: column_name, 'Frecuencia': 'Frecuencia'},
-                 color='Frecuencia',  # Opcional: para dar color a las barras según la frecuencia
-                 color_continuous_scale='Viridis')  # Opcional: escala de colores
+    fig = px.bar(data, x=list_cat, y='Frecuencia',
+                 title=f'Distribución de la Variable Categórica: {list_cat}',
+                 labels={list_cat: list_cat, 'Frecuencia': 'Frecuencia'},
+                 color='Frecuencia',  # dar color a las barras según la frecuencia
+                 color_continuous_scale='Viridis')  # escala de colores
 
     # Ajustar la apariencia del gráfico
     fig.update_layout(template='simple_white', title_x=0.5, showlegend=False)
@@ -223,12 +135,8 @@ def plot_categorical_distribution(df, column_name):
 #Función para el gráfico de la variable de respuesta
 def plot_renuncia_2016(df, columna_renuncia, titulo='Cantidad de Renuncias en 2016'):
     """
-    Genera un gráfico de barras que muestra la cantidad de renuncias en 2016.
-
-    Parámetros:
-    df (pd.DataFrame): DataFrame que contiene los datos.
-    columna_renuncia (str): Nombre de la columna que contiene la información de renuncias.
-    titulo (str): Título del gráfico (opcional).
+    Genera un gráfico de barras que muestra la cantidad de renuncias en 2016, recibe como argumentos un
+    dataframe, el nombre de la variable renuncia y por último un titulo.
     """
     # Contar la cantidad de renuncias
     conteo_renuncia = df[columna_renuncia].value_counts()
@@ -251,22 +159,18 @@ def plot_renuncia_2016(df, columna_renuncia, titulo='Cantidad de Renuncias en 20
 
     # Mostrar el gráfico
     plt.show()
-    return None
 
-#Gráficos de variables categóricas vs variable de respuesta
-def plot_categorical_vs_binary(df, v_respuesta, categorical_cols):
-    for col in categorical_cols:
-        plt.figure(figsize=(10, 6))
-        sns.countplot(x=col, hue=v_respuesta, data=df, palette='viridis')
-        plt.title(f'Distribución de {v_respuesta} por {col}')
-        plt.xticks(rotation=45, ha="right")
-        plt.show()
-
-# Función de Matriz de correlación v numéricas
-
-def plot_correlation_matrix(df, columns_num):
+# Función de Matriz de correlación v_numéricas
+def plot_correlation_matrix(df, list_num):
+    '''
+    Esta función genera una matriz de correlación que puede observarse por medio de un mapa de calor,
+    es idea para analizar si existe multicolinealidad entre las variables.
+    Recibe como argumentos:
+    - df: dataframe con los datos.
+    - list_num: lista de los nombres de las variables numéricas
+    '''
     # Calcula la matriz de correlación
-    matriz_correlacion = df[columns_num].corr()
+    matriz_correlacion = df[list_num].corr()
 
     # Visualizar la matriz de correlación
     plt.figure(figsize=(15, 12), dpi=80)
@@ -274,32 +178,68 @@ def plot_correlation_matrix(df, columns_num):
     plt.title("Mapa de calor de la matriz de correlación")
     plt.show()
 
-#----------------- Métodos de filtrado --------------------------
+#Gráficos de variables categóricas vs variable de respuesta
+def plot_categorical_vs_binary(df, v_respuesta, list_cat):
+    '''
+    Esta función genera un gráfico de histograma para cada variable categórica vs la variable de respuesta,
+    por ello recibe como argumentos:
+    - df: un dataframe con los datos.
+    - v_respuesta: Nombre de la variable de respuesta.
+    - list_cat: Lista de las variables categóricas
 
+    Esto se hace con el fin de observar que comportamientos tiene la variable de respuesta respecto
+    a estas variables categóricas.
+    '''
+    for col in list_cat:
+        plt.figure(figsize=(10, 6))
+        sns.countplot(x=col, hue=v_respuesta, data=df, palette='viridis')
+        plt.title(f'Distribución de {v_respuesta} por {col}')
+        plt.xticks(rotation=45, ha="right")
+        plt.show()
+
+
+#--------------------Escalado de variables-------------------------------
 #Función para el escalado de los datos
 def escalar_datos(v_num):
+    '''
+    Esta función escala los datos numéricos para ser utilizados en los modelos.
+    Recibe como argumento un dataframe con variables numéricas y retorna un dataframe 
+    con las variables escaladas con el standarscaler.
+    '''
     sc = StandardScaler()
-    
     x_sc = sc.fit_transform(v_num)
 
     df_x_sc = pd.DataFrame(x_sc, columns=v_num.columns, index=v_num.index)
-    
+
     return df_x_sc
 
+#--------------------Selección de variables-------------------------------
 # Función recursiva de selección de características
-def recursive_feature_selection(X,y,model,k): # model=modelo que me va a servir de estimador en este caso de regresión logística
-    rfe = RFE(model, n_features_to_select=k, step=1)# step=1 cada cuanto el toma la sucesión de tomar una caracteristica
+def recursive_feature_selection(X,y,model,k):
+    '''
+    Esta función tiene como objetivo realizar la selección de variables por medio de la función
+    RFE, para ello recive 4 argumentos:
+    - X: Datos de entrenamiento
+    - y: Datos de prueba
+    - model: El modelo entrenado para seleccionar las variables
+    - k: El número de variables a seleccionar
+
+    Finalmente retorna X_new que es el dataframe que contiene las k variables que más explican el modelo.
+    '''
+    rfe = RFE(model, n_features_to_select=k, step=1)
     fit = rfe.fit(X, y)
     X_new = fit.support_
-    print("Num Features: %s" % (fit.n_features_))
-    print("Selected Features: %s" % (fit.support_))
-    print("Feature Ranking: %s" % (fit.ranking_))
-
     return X_new
 
+#------------------------ Tratamiento de datos -----------------------------
 # Función para el tratamiento de datos
 def preparar_datos(df):
-
+    '''
+    Esta función tiene el fin de preparar los datos para el modelado y poder realizar las 
+    predicciones del siguiente año. Para ello recive como argumento df, el dataframe del 
+    año presente y con los datos los cuales se realizarán las predicciones.
+    Retorna df_final que es el dataframe con las variables escaladas y seleccionadas para el modelado.
+    '''
     #### Cargar listas y scaler
     list_num = joblib.load("salidas\\list_num.pkl")  # Lista de variables numéricas
     list_dummi = joblib.load("salidas\\list_dummi.pkl")  # Lista de variables categóricas para dummizar
@@ -308,6 +248,7 @@ def preparar_datos(df):
 
     # 1. Eliminar filas con datos faltantes
     df = df.dropna()
+
     # 2. Transformar variables 'EmployeeID' y 'PercentSalaryHike'
     df['EmployeeID'] = df['EmployeeID'].astype(str)  # Convertir 'EmployeeID' a string
     df['PercentSalaryHike'] = df['PercentSalaryHike'] / 100  # Escalar 'PercentSalaryHike' dividiendo por 100
@@ -333,16 +274,17 @@ def preparar_datos(df):
 
     return df_final
 
+#------------------------ Despliegue de resultados -----------------------------
+#Función para conocer el rol con más retiros
 def empleados_criticos(df_predic, job_roles):
     """
     Identifica el rol con el mayor promedio de renuncias y exporta los empleados que se van a retirar.
-    
-    Args:
+    Recibe como argumentos un dataframe de predicciones y la lista de los jobs_roles que se observaron en 
+    el análisis exploratorio.
         perf_pred (pd.DataFrame): DataFrame que contiene los datos de empleados.
         job_roles (list): Lista de nombres de columnas para los roles de trabajo.
     
-    Returns:
-        None
+    Retorna: el rol con el mayor número de renuncias.
     """
     # Crear un diccionario para almacenar el promedio de renuncias por rol
     promedio_renuncias = {}
@@ -357,17 +299,6 @@ def empleados_criticos(df_predic, job_roles):
     
     # Encontrar el rol con el mayor promedio de renuncias
     rol_mayor_renuncia = max(promedio_renuncias, key=promedio_renuncias.get)
+
     return rol_mayor_renuncia
-    print(f"El rol con mayor promedio de renuncias es: {rol_mayor_renuncia}")
-    
-    # Filtrar datos por el rol con mayor promedio de renuncias
-    empleados_criticos_mayor_renuncia = perf_pred[perf_pred[rol_mayor_renuncia] == 1]
-    empleados_criticos_mayor_renuncia = empleados_criticos_mayor_renuncia[
-        empleados_criticos_mayor_renuncia['pred_renuncia_2017'] == 1
-    ]
-    
-    # Exportar los IDs de los empleados y la variable pred_renuncia_2017
-    archivo_salida = f"salidas/empleados_criticos_{rol_mayor_renuncia.replace(' ', '_').replace('JobRole_', '')}.xlsx"
-    empleados_criticos_mayor_renuncia[['EmployeeID', 'pred_renuncia_2017']].to_excel(archivo_salida, index=False)
-    print(f"Archivo exportado: {archivo_salida}")
 
